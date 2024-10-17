0e9be2b28a80f72f4c65b9886b4ab1c889350a1a
Jerome <esung246@gmail.com>
2024-10-17 17:07:35 +0900

    fix

--- index.js
+++ index.js
@@ -4,15 +4,30 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-app.use(cors());
-
+app.use(
+    cors({
+      origin: ['http://localhost:3000', 'https://zero3062.github.io/Eternal-Return-League'],
+    })
+  );
 const server = http.createServer(app);
 
 var io = new Server(server, {
-  cors: {
-    origin: "https://zero3062.github.io/Eternal-Return-League",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: (origin, callback) => {
+          const allowedOrigins = [
+            "https://zero3062.github.io/Eternal-Return-League",
+            "http://localhost:3000"
+          ];
+    
+          if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            // 허용된 오리진이거나 origin이 없으면 허용
+            callback(null, true);
+          } else {
+            callback(new Error('Not allowed by CORS'));
+          }
+        },
+        methods: ["GET", "POST"],
+    },
 });
 
 io.on("connection", (socket) => {
