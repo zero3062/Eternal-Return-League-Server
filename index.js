342d5c86608a7fe935df27decadf359994c91a34
zero3062 <esung246@gmail.com>
2024-10-17 21:51:00 +0900

    fix : cluster 제거(생각을 해봐야겠음)

--- index.js
+++ index.js
@@ -1,82 +1,40 @@
 const express = require('express');
-const cors = require('cors');
-const cluster = require('cluster');
+const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
-const redisAdapter = require('socket.io-redis');
-const numCPUs = require('os').cpus().length;
-const { setupMaster, setupWorker } = require('@socket.io/sticky');
+const cors = require('cors');
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://zero3062.github.io/Eternal-Return-League',
+    ],
+  })
+);
+const server = http.createServer(app);
 
-  const app = express();
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'https://zero3062.github.io/Eternal-Return-League',
-      ],
-    })
-  );
+var io = new Server(server, {
+  cors: {
+    origin: [
+      'http://localhost:3000',
+      'https://zero3062.github.io/Eternal-Return-League',
+    ],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+  allowEIO3: true,
+});
 
-  const server = http.createServer(app);
-  setupMaster(server, {
-    loadBalancingMethod: 'least-connection', // either "random", "round-robin" or "least-connection"
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('send_message', (data) => {
+    console.log(data);
+    socket.broadcast.emit('receive_message', data);
   });
+});
 
-  server.listen(8080, () => {
-    console.log(`Worker ${process.pid} started and SERVER IS RUNNING`);
-  });
-
-  // 각 CPU에 대해 워커 생성
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  // 워커가 종료되면 로그를 남기고 새로운 워커를 생성
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    cluster.fork();
-  });
-} else {
-  const app = express();
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'https://zero3062.github.io/Eternal-Return-League',
-      ],
-    })
-  );
-
-  const server = http.createServer(app);
-
-  const io = new Server(server, {
-    cors: {
-      origin: [
-        'http://localhost:3000',
-        'https://zero3062.github.io/Eternal-Return-League',
-      ],
-      methods: ['GET', 'POST'],
-      credentials: true,
-    },
-    allowEIO3: true,
-  });
-
-  io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
-  setupWorker(io);
-
-  io.on('connection', (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
-    socket.on('send_message', (data) => {
-      console.log(data);
-      socket.broadcast.emit('receive_message', data);
-    });
-  });
-
-  server.listen(8080, () => {
-    console.log(`Worker ${process.pid} started and SERVER IS RUNNING`);
-  });
-}
+server.listen(8080, () => {
+  console.log('SERVER IS RUNNING');
+});
