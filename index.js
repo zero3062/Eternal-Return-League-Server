1204435704219f25f7d06d6d04f6eb3aed3f6ed7
Jerome <esung246@gmail.com>
2024-10-17 16:47:12 +0900

    fix : setting

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 var io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://zero3062.github.io/Eternal-Return-League",
     methods: ["GET", "POST"],
   },
 });
