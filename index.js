d1b4e37f83e2e571e8208db29515f9874fd14378
zero3062 <esung246@gmail.com>
2024-10-17 20:44:57 +0900

    fix: test

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
 app.use(
-    cors({
-      origin: ['http://localhost:3000', 'https://zero3062.github.io/Eternal-Return-League'],
-    })
-  );
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://zero3062.github.io/Eternal-Return-League',
+    ],
+  })
+);
 const server = http.createServer(app);
 
 var io = new Server(server, {
-    cors: {
-        origin: [
-            "https://zero3062.github.io/Eternal-Return-League",
-            "http://localhost:3000"
-        ],
-        methods: ["GET", "POST"],
-        transports: ['websocket', 'polling'],
-        credentials: true
-    },
-    allowEIO3: true
+  cors: {
+    origin: [
+      'http://localhost:3000',
+      'https://zero3062.github.io/Eternal-Return-League',
+    ],
+    methods: ['GET', 'POST'],
+    transports: ['websocket', 'polling'],
+    credentials: true,
+  },
+  allowEIO3: true,
 });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("send_message", (data) => {
-    console.log(data)
-    socket.broadcast.emit("receive_message", data); 
+  socket.on('send_message', (data) => {
+    console.log(data);
+    socket.broadcast.emit('receive_message', data);
   });
 });
 
 server.listen(8080, () => {
-  console.log("SERVER IS RUNNING");
+  console.log('SERVER IS RUNNING');
 });
