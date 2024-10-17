0020b829cb3cdec32a34f64ebd759a082254b534
Jerome <esung246@gmail.com>
2024-10-17 16:36:54 +0900

    feat: sestting

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+var io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    console.log(data)
+    socket.broadcast.emit("receive_message", data); 
+  });
+});
+
+server.listen(8080, () => {
+  console.log("SERVER IS RUNNING");
+});
